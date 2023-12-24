8f77f3b13839678f2d925b78effd61262087208b
praneethprogit <85063680+praneethprogame@users.noreply.github.com>
2023-12-24 17:47:45 +0530

    Update UptimePlugin.java

--- src/main/java/pran/plugins/UptimePlugin.java
+++ src/main/java/pran/plugins/UptimePlugin.java
@@ -13,14 +13,15 @@ import java.util.Date;
 
 public class UptimePlugin extends JavaPlugin {
     private long startTime;
+    private int startTimeAfterSeconds;
 
     @Override
     public void onEnable() {
-        System.out.println("Starting Uptime record");
+        loadConfig();
         if (getConfig().contains("startTime")) {
             startTime = getConfig().getLong("startTime");
         } else {
-            startTime = System.currentTimeMillis();
+            startTime = System.currentTimeMillis() + (startTimeAfterSeconds * 1000); // Add delay if specified
             getConfig().set("startTime", startTime);
             saveConfig();
         }
@@ -29,15 +30,24 @@ public class UptimePlugin extends JavaPlugin {
             long previousUptime = readPreviousUptime();
             sendUptimeMessage(previousUptime);
         }
+        System.out.println("Starting Uptime record");
     }
 
     @Override
     public void onDisable() {
-
         System.out.println("Stoping Uptime record");
         System.out.println("Writing to Log file");
+
         long uptime = System.currentTimeMillis() - startTime;
         writeUptimeToLogFile(uptime);
+    }
+
+    private void loadConfig() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        // Load the configuration values
+        startTimeAfterSeconds = getConfig().getInt("start-time-after", 0);
     }
 
     private boolean previousLogFileExists() {
@@ -60,60 +70,33 @@ public class UptimePlugin extends JavaPlugin {
     }
 
     private void writeUptimeToLogFile(long uptime) {
-    try {
-        File folder = getDataFolder();
-        if (!folder.exists()) {
-            folder.mkdir();
-        }
-
-        // Create a SimpleDateFormat object with the desired date format
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        String formattedDate = dateFormat.format(new Date());
-
-        // Create a SimpleDateFormat object with the desired time format
-        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-        String formattedTime = timeFormat.format(new Date());
-
-        // Combine the formatted date and time for the log entry
-        String logEntry = String.format("Server uptime for %s (%s):\n", formattedDate, formattedTime);
-        logEntry += String.format("              %d hours\n", uptime / 3600000); // Convert milliseconds to hours
-        logEntry += String.format("              %d minutes\n", (uptime % 3600000) / 60000); // Convert remaining milliseconds to minutes
-        logEntry += String.format("              %d seconds\n", (uptime % 60000) / 1000); // Convert remaining milliseconds to seconds
-
-        // Read existing content from the log file
-        StringBuilder existingContent = new StringBuilder();
-        if (folder.exists()) {
-            File logFile = new File(folder, "uptime.log");
-            if (logFile.exists()) {
-                // Read existing content from the log file
-                BufferedReader reader = new BufferedReader(new FileReader(logFile));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    existingContent.append(line).append("\n");
-                }
-                reader.close();
+        try {
+            File folder = getDataFolder();
+            if (!folder.exists()) {
+                folder.mkdir();
             }
-        }
 
-        // Write the new log entry followed by existing content
-        BufferedWriter writer = new BufferedWriter(new FileWriter(new File(folder, "uptime.log")));
-        writer.write(logEntry);
-        writer.write(existingContent.toString());
-        writer.close();
-    } catch (IOException e) {
-        e.printStackTrace();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File(folder, "uptime.log")));
+            writer.write(formatUptime(uptime));
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
 
     private void sendUptimeMessage(long uptime) {
-        getLogger().info("Previous server uptime: " + formatUptime(uptime));
+        getLogger().info("Server uptime of " + formatDate() + ":\n"
+                + "             " + (uptime / 3600000) + " hours\n"
+                + "             " + ((uptime % 3600000) / 60000) + " minutes");
+    }
+
+    private String formatDate() {
+        return new java.text.SimpleDateFormat("MM/dd/yy").format(new java.util.Date());
     }
 
     private String formatUptime(long uptime) {
-        long seconds = uptime / 1000;
-        long minutes = seconds / 60;
-        long hours = minutes / 60;
-
-        return String.format("%02d:%02d:%02d", hours, minutes % 60, seconds % 60);
+        return formatDate() + ":\n" +
+                "             " + (uptime / 3600000) + " hours\n" +
+                "             " + ((uptime % 3600000) / 60000) + " minutes";
     }
 }
