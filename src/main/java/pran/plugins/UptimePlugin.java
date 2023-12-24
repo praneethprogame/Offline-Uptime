8770b13dbcb3c9753119e6bc52735fbe96ff371d
praneethprogit <85063680+praneethprogame@users.noreply.github.com>
2023-12-24 11:15:54 +0530

    Update UptimePlugin.java

--- src/main/java/pran/plugins/UptimePlugin.java
+++ src/main/java/pran/plugins/UptimePlugin.java
@@ -2,7 +2,14 @@ package pran.plugins;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class UptimePlugin extends JavaPlugin {
     private long startTime;
@@ -53,19 +60,50 @@ public class UptimePlugin extends JavaPlugin {
     }
 
     private void writeUptimeToLogFile(long uptime) {
-        try {
-            File folder = getDataFolder();
-            if (!folder.exists()) {
-                folder.mkdir();
-            }
-
-            BufferedWriter writer = new BufferedWriter(new FileWriter(new File(folder, "uptime.log")));
-            writer.write(formatUptime(uptime));
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    try {
+        File folder = getDataFolder();
+        if (!folder.exists()) {
+            folder.mkdir();
         }
+
+        // Create a SimpleDateFormat object with the desired date format
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String formattedDate = dateFormat.format(new Date());
+
+        // Create a SimpleDateFormat object with the desired time format
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        String formattedTime = timeFormat.format(new Date());
+
+        // Combine the formatted date and time for the log entry
+        String logEntry = String.format("Server uptime for %s (%s):\n", formattedDate, formattedTime);
+        logEntry += String.format("              %d hours\n", uptime / 3600000); // Convert milliseconds to hours
+        logEntry += String.format("              %d minutes\n", (uptime % 3600000) / 60000); // Convert remaining milliseconds to minutes
+        logEntry += String.format("              %d seconds\n", (uptime % 60000) / 1000); // Convert remaining milliseconds to seconds
+
+        // Read existing content from the log file
+        StringBuilder existingContent = new StringBuilder();
+        if (folder.exists()) {
+            File logFile = new File(folder, "uptime.log");
+            if (logFile.exists()) {
+                // Read existing content from the log file
+                BufferedReader reader = new BufferedReader(new FileReader(logFile));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    existingContent.append(line).append("\n");
+                }
+                reader.close();
+            }
+        }
+
+        // Write the new log entry followed by existing content
+        BufferedWriter writer = new BufferedWriter(new FileWriter(new File(folder, "uptime.log")));
+        writer.write(logEntry);
+        writer.write(existingContent.toString());
+        writer.close();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+}
 
     private void sendUptimeMessage(long uptime) {
         getLogger().info("Previous server uptime: " + formatUptime(uptime));
